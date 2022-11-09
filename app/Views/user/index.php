388f425f556efc8e74622dfbb9c64eb2dc20a8ba
Fadil <fadilrahman0320@gmail.com>
2022-11-08 21:56:33 -0800

    init push

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -9,5 +9,41 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-<h1>Daftar User</h1>
-<?= $this->endSection() ?>
+<div class="card">
+    <div class="card-header">
+        <div class="d-flex justify-content-between">
+            <h3>Daftar User</h3>
+            <a href="<?= base_url('user/tambah') ?>" class="btn btn-info">Tambah</a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Username</th>
+                        <th>Nama Lengkap</th>
+                        <th>NIK</th>
+                        <th>Kewarganegaraan</th>
+                        <th>E-Mail</th>
+                        <th>No.Hp</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>001</td>
+                        <td>Testing Kamar</td>
+                        <td>1273623393924</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <?= $this->endSection() ?>
